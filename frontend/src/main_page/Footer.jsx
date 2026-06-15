e51049131c4f22400b6e9e5f2f728f3557776fad
James Nicholls <james@kert.com.au>
2026-06-15 14:04:09 +1000

    add space before James

--- frontend/src/main_page/Footer.jsx
+++ frontend/src/main_page/Footer.jsx
@@ -2,7 +2,7 @@
 export function Footer() {
     return (
         <p style={{position : "absolute", bottom : "0"}}>Created by 
-            <a href="https://www.linkedin.com/in/james-nicholls-014b20293/" target="_blank">James Nicholls</a>, 2026
+            <a href="https://www.linkedin.com/in/james-nicholls-014b20293/" target="_blank"> James Nicholls</a>, 2026
         </p>
     )
 }
